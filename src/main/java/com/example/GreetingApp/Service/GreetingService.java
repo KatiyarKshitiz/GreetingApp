5ab0ee0ea0e2b7bd3f719555193a74845f485056
Kshitiz Katiyar <katiyarkshitiz0912@gmail.com>
2025-03-05 22:46:11 +0530

    Greeting app with deleteGreeting

--- src/main/java/com/example/GreetingApp/Service/GreetingService.java
+++ src/main/java/com/example/GreetingApp/Service/GreetingService.java
@@ -1,4 +1,3 @@
-
 package com.example.GreetingApp.Service;
 import com.example.GreetingApp.Model.Greeting;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -58,6 +57,14 @@ public class GreetingService {
             Greeting greeting = existingGreeting.get();
             greeting.setMessage(newMessage);  // Update message
             return greetingRepository.save(greeting);  // Save updated greeting
+        } else {
+            throw new RuntimeException("Greeting with ID " + id + " not found.");
+        }
+    }
+
+    public void deleteGreetingById(Long id) {
+        if (greetingRepository.existsById(id)) {
+            greetingRepository.deleteById(id);
         } else {
             throw new RuntimeException("Greeting with ID " + id + " not found.");
         }
